9afb434fd76049c962bab77845568bbb32531e88
INiiko S <112642927+INiiko@users.noreply.github.com>
2024-12-16 12:59:33 -0300

    Siguiente Commit

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/Godesde0/variables"
+)
+
+func main() {
+	estado, texto := variables.ConviertoaTexto(1588)
+	fmt.Println(estado)
+	fmt.Println(texto)
+}

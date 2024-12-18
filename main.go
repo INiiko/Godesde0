cf1b6255acd5a0ab20bd87761e3b1f8c6894c2eb
INiiko S <112642927+INiiko@users.noreply.github.com>
2024-12-17 22:25:48 -0300

    Ejercicio01 - Test

--- main.go
+++ main.go
@@ -2,31 +2,18 @@ package main
 
 import (
 	"fmt"
-	"runtime"
-	"\ejercicios"
+
+	"github.com/Godesde0/ejercicios"
 )
 
+// var Numero = 500 ConvNumerico("500")
 func main() {
-	/* estado, texto := variables.ConviertoaTexto(1588)
-	fmt.Println(estado)
+	// estado, texto := variables.ConviertoaTexto(1588)
+	// fmt.Println(estado)
+	// fmt.Println(texto)
+
+	numero, texto := ejercicios.ConvNumerico("50")
+	fmt.Println(numero)
 	fmt.Println(texto)
 
-	if os := runtime.GOOS; os == "linux" {
-		fmt.Println("Esto no es Windows")
-	} else {
-		fmt.Println("Esto es Windows")
-	}
-
-	switch os := runtime.GOOS; os {
-	case "linux":
-		fmt.Println("Esto es Linux")
-	case "darwin":
-		fmt.Println("Esto es darwin")
-	default:
-		fmt.Printf("%s \n", os)
-	}
-*/
-		numero, texto := ejercicios.ConvNumerico("500")
-		fmt.println(numero)
-		fmt.Println(texto)
 }
